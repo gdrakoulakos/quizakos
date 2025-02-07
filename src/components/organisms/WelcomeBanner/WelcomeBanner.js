24972c7aa9ffe17c37a098b3f4133116f93cd06d
gdrakoulakos <georgediopan@gmail.com>
2025-02-08 01:41:12 +0200

    Correction on Welcome banner

--- src/components/organisms/WelcomeBanner/WelcomeBanner.js
+++ src/components/organisms/WelcomeBanner/WelcomeBanner.js
@@ -4,7 +4,7 @@ export default function WelcomeBanner() {
   return (
     <div className={styles.welcomeBanner}>
       <h1>Welcome to forteQuiz</h1>
-      <p1>Play some Quizzes and test your music skills</p1>
+      <p>Play some Quizzes and test your music skills</p>
     </div>
   );
 }
