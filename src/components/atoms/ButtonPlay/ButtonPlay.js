62c4f425c4bd7397ae1e8782313145fc8b805214
gdrakoulakos <georgediopan@gmail.com>
2025-02-11 20:28:08 +0200

    Make button play to navigate to quiz page

--- src/components/atoms/ButtonPlay/ButtonPlay.js
+++ src/components/atoms/ButtonPlay/ButtonPlay.js
@@ -1,9 +1,12 @@
+import Link from "next/link"
 import styles from "../ButtonPlay/ButtonPlay.module.css"
 
 export default function ButtonPlay () {
     return(
         <div className={styles.buttonContainer}>
+            <Link href={"/quiz"}>
             <button>Play Now</button>
+            </Link>
         </div>
     )
 }
