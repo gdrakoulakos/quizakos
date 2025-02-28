8b9c31ebed265265f6ff86e11524551dd50f0181
gdrakoulakos <georgediopan@gmail.com>
2025-02-28 23:32:29 +0200

    Create context from History category

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,43 +1,28 @@
 const { createContext, useContext, useState } = require("react");
 import notationAndStructure from "../data/quizQuestions/theory/notationAndStructure";
 import chordsAndHarmony from "../data/quizQuestions/theory/chordsAndHarmony";
-
+import baroqueAndClassicalEras from "../data/quizQuestions/history/baroqueAndClassicalEras";
+import romanticAndModernEras from "../data/quizQuestions/history/romanticAndModernEras";
 
 const allQuizData = {
-    notationAndStructure: notationAndStructure,
-    chordsAndHarmony: chordsAndHarmony
-}
+  notationAndStructure: notationAndStructure,
+  chordsAndHarmony: chordsAndHarmony,
+  baroqueAndClassicalEras: baroqueAndClassicalEras,
+  romanticAndModernEras: romanticAndModernEras,
+};
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-    const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("");
 
-    const quizData = allQuizData[category];
+  const quizData = allQuizData[category];
 
-
-  return <AppContext.Provider value={{category, setCategory, quizData}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ category, setCategory, quizData }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const QuizContext = () => useContext(AppContext);
-
-
-
-
-
-
-
-
-// const { setCategory } = QuizzContext();
-
-// onClick = () => setCategory('mousikh')
-
-
-
-
-
-
-
-// const { questions } = QuizzContext();
-
-// console.log(questions);
