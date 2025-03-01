0fa6b8e91887af71e121bc15e739286217db6c12
gdrakoulakos <georgediopan@gmail.com>
2025-03-01 21:53:54 +0200

    Quizzes component will be used for all quiz categories

---
+++ src/components/organisms/Quizzes/Quizzes.js
@@ -0,0 +1,23 @@
+import CardQuiz from "@/components/molecules/CardQuiz/CardQuiz";
+import styles from "../Quizzes/Quizzes.module.css";
+import { QuizContext } from "@/context/AppContext";
+
+export default function Quizzes({ category }) {
+  const { allQuizzes } = QuizContext();
+  const quiz = allQuizzes.filter((q) => q.category === category);
+  return (
+    <>
+      <h1>{category}</h1>
+      <div className={styles.cardsContainer}>
+        {quiz.map((card) => (
+          <CardQuiz
+            key={card.id}
+            quizCategory={card.quizCategory}
+            name={card.subcategory}
+            description={card.description}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
