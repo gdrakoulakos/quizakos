bd1b3f880ee0d7e5e28330a8ef366244a6784d58
George Drakoulakos <g.drakoulakos@liquid.gr>
2025-12-28 10:35:21 +0200

    Refactor layout styles: move body styles from inline to globals.css for better maintainability

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,21 +9,7 @@ import Footer from "@/components/organisms/Footer/Footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="el">
-      <body
-        className={commissioner.className}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "100vw",
-          overflowX: "hidden",
-          padding: "0",
-          margin: "0",
-          backgroundImage: "url('/images/background2.webp')",
-          backgroundSize: "contain",
-          backgroundAttachment: "fixed",
-        }}
-      >
+      <body className={commissioner.className}>
         <CookiesProvider>
           <AppProvider>
             <Header />
