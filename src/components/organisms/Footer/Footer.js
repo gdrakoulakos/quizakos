eb274c77b9b219edb11a2edee74a8f5cf25c11ba
George Drakoulakos <g.drakoulakos@liquid.gr>
2025-12-30 12:50:04 +0200

    Change CSS styling on footer

--- src/components/organisms/Footer/Footer.js
+++ src/components/organisms/Footer/Footer.js
@@ -3,7 +3,9 @@ import styles from "../Footer/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© {new Date().getFullYear()} forteQuiz. All Rights Reserved.</p>
+      <p>
+        © {new Date().getFullYear()} George Drakoulakos. All Rights Reserved.
+      </p>
     </footer>
   );
 }
