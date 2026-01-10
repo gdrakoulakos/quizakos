a40051ed2397c9c1389e73c1d3e42e1ad3119f1c
George Drakoulakos <g.drakoulakos@liquid.gr>
2026-01-10 20:44:51 +0200

    Add @clerk/localizations dependency and integrate Greek localization in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import { CookiesProvider } from "react-cookie";
 import Header from "@/components/organisms/Header/Header";
 import Footer from "@/components/organisms/Footer/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
+import { elGR } from "@clerk/localizations";
 
 export default function RootLayout({ children }) {
   return (
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body className={commissioner.className}>
         <ClerkProvider
           publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+          localization={elGR}
         >
           <CookiesProvider>
             <AppProvider>
