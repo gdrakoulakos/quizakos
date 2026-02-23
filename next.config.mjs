ab6dc89b468dfb8cd3dd3be820dfd7885fb85abd
George Drakoulakos <g.drakoulakos@liquid.gr>
2026-02-23 23:54:23 +0200

    Add redirect configuration for quizakos.gr to enforce HTTPS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "quizakos.gr",
+          },
+        ],
+        destination: "https://www.quizakos.gr/:path*",
+        permanent: true, // 301
+      },
+    ];
+  },
+};
 
 export default nextConfig;
