8a08aa48023ddbda0fefdfd876d8b619eab99af3
george <g.drakoulakos@liquid.gr>
2026-04-06 16:47:06 +0300

    Remove redirect configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "quizakos.gr",
-          },
-        ],
-        destination: "https://www.quizakos.gr/:path*",
-        permanent: true, // 301
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
