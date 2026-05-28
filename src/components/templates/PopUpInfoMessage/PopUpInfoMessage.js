9a04a52fe0c3b9c2f6fa82582e11b62e52dadac7
George Drakoulakos <g.drakoulakos@liquid.gr>
2026-05-28 18:31:50 +0300

    Add copy-to-clipboard functionality to PopUpInfoMessage component and update styles

--- src/components/templates/PopUpInfoMessage/PopUpInfoMessage.js
+++ src/components/templates/PopUpInfoMessage/PopUpInfoMessage.js
@@ -1,9 +1,13 @@
 import ButtonYesNo from "@/components/atoms/ButtonYesNo/ButtonYesNo";
 import styles from "./PopUpInfoMessage.module.css";
 import { QuizContext } from "@/context/AppContext";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { useState } from "react";
 
 export default function PopUpInfoMessage({ message, action }) {
   const { setShowPopUpInfoMessage, displayedQuestionId } = QuizContext();
+  const [isIdCopied, setIsIdCopied] = useState(false);
 
   const handleButtonClick = () => {
     setShowPopUpInfoMessage(false);
@@ -13,7 +17,18 @@ export default function PopUpInfoMessage({ message, action }) {
     <div className={styles.popupWrapper}>
       <div className={styles.popupContent}>
         <p className={styles.message}>{message}</p>
-        <p className={styles.questionId}>{displayedQuestionId}</p>
+        <div className={styles.questionIdContainer}>
+          <p className={styles.questionId}>{displayedQuestionId}</p>
+          <CopyToClipboard
+            text={displayedQuestionId}
+            onCopy={() => setIsIdCopied(true)}
+          >
+            <ContentCopyIcon fontSize="small" />
+          </CopyToClipboard>
+        </div>
+        {isIdCopied && (
+          <span className={styles.copiedText}>Το ID αντιγράφηκε!</span>
+        )}
         <div className={styles.buttonContainer}>
           <ButtonYesNo
             handleButtonClick={() => handleButtonClick()}
