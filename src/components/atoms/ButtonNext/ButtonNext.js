92bf6d9bcff78b9fea3afef49cdc8942d204ae22
george <g.drakoulakos@liquid.gr>
2026-05-29 10:57:08 +0300

    Refactor ButtonNext component to use MUI icon and simplify structure; adjust styles in ButtonNext and PopUpResults components

--- src/components/atoms/ButtonNext/ButtonNext.js
+++ src/components/atoms/ButtonNext/ButtonNext.js
@@ -1,25 +1,18 @@
 import styles from "./ButtonNext.module.css";
 import { motion } from "motion/react";
-import Image from "next/image";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 
-export default function ButtonNext({ buttonText, onClick }) {
+export default function ButtonNext({ onClick }) {
   return (
     <motion.div
-      key={buttonText}
+      key="next"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5, delay: 0.3 }}
       className={styles.buttonContainer}
       onClick={onClick}
     >
-      <Image
-        src={"/images/next.svg"}
-        alt="next question"
-        width={50}
-        height={50}
-        className={styles.buttonImage}
-      />
-      <div className={styles.buttonText}>{buttonText}</div>
+      <ArrowForwardIosRoundedIcon className={styles.buttonImage} />
     </motion.div>
   );
 }
