012ac349594a1a7e6ea6442fb7a6ebc2d2c27211
Drakoulakos, Georgios <DrakoulakosG@kotsovolos.gr>
2026-08-18 19:36:40 +0300

    Replace createBrowserClient with createClient for improved consistency and functionality in auth callback

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -1,4 +1,4 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
@@ -6,10 +6,15 @@ export async function GET(request) {
 
   const code = searchParams.get("code");
 
-  if (code) {
-    const supabase = await createBrowserClient();
+  console.log("CODE EXISTS:", !!code);
 
-    await supabase.auth.exchangeCodeForSession(code);
+  if (code) {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+
+    console.log("EXCHANGE ERROR:", error);
+    console.log("SESSION EXISTS:", !!data?.session);
   }
 
   return NextResponse.redirect(new URL("/?loginSuccess=true", request.url));
