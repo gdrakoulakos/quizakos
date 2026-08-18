7a3c5a5fad643e9bd7cb34c354d89125e3dd29d7
Drakoulakos, Georgios <DrakoulakosG@kotsovolos.gr>
2026-08-18 18:27:49 +0300

    Refactor GET function in route.js to set login success cookie and improve redirect logic

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -2,6 +2,8 @@ import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
+  console.log("hello");
+
   const { searchParams } = new URL(request.url);
 
   const code = searchParams.get("code");
@@ -12,5 +14,12 @@ export async function GET(request) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(new URL("/?loginSuccess=true", request.url));
+  const response = NextResponse.redirect(new URL("/", request.url));
+
+  response.cookies.set("login_success", "true", {
+    maxAge: 10,
+    path: "/",
+  });
+
+  return response;
 }
